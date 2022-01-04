77e8157bc428b975303f683593e00ac3016b9755
Richard Tracy <20599825+PowerShellCrack@users.noreply.github.com>
2022-01-04 07:36:04 -0500

    fixed output

--- AddLocalAdminObfuscated.ps1
+++ AddLocalAdminObfuscated.ps1
@@ -14,33 +14,32 @@
 	Source	    : https://github.com/PowerShellCrack/PSAutopilotLocalAdmin
     Version		: 1.0.0
     #Requires -Version 3.0
-#>
 
-<#
-##*=============================================
-## !!! IMPORTANT !!! STORE THESE STEPS ELSEWHERE
-##*=============================================
+    ##*=============================================
+    ## !!! IMPORTANT !!!
+    ##*=============================================
+    STORE THESE STEPS ELSEWHERE, run them, then update variables in VARIABLE section
 
-#How to "Obfuscate" password (encrypt & decrypt)
+    #How to "Obfuscate" password (encrypt & decrypt)
 
-#STEP 1 - create random passphase (256 AES). Save the output as a variable (copy/paste)
-#NOTE: this key is unique; the same key must be used to decrypt
-$AESKey = New-Object Byte[] 32
-[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($AESKey)
-Write-host ('$AESKey = @(' + ($AESKey -join ",").ToString() + ')')
+    #STEP 1 - create random passphase (256 AES). Save the output as a variable (copy/paste)
+    #NOTE: this key is unique; the same key must be used to decrypt
+    $AESKey = New-Object Byte[] 32
+    [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($AESKey)
+    Write-host ('$AESKey = @(' + ($AESKey -join ",").ToString() + ')')
 
-#STEP 2 - Encrypt password with AES key. Save the output as a variable (copy/paste)
-$UserPassword = 'P@ssw0rd12!'
-$UserObfuscatedPassword = ConvertTo-SecureString -String $UserPassword -AsPlainText -Force | ConvertFrom-SecureString -Key $AESKey
-Write-host ('$UserObfuscatedPassword = "' + $UserObfuscatedPassword + '"')
+    #STEP 2 - Encrypt password with AES key. Save the output as a variable (copy/paste)
+    $UserPassword = 'P@ssw0rd12!'
+    $UserObfuscatedPassword = ConvertTo-SecureString -String $UserPassword -AsPlainText -Force | ConvertFrom-SecureString -Key $AESKey
+    Write-host ('$UserObfuscatedPassword = "' + $UserObfuscatedPassword + '"')
 
-#STEP 3 - Store as useable credentials; converts encrypted key into secure key for use (used in the script)
-$UserName = 'IntuneAdmin'
-$SecurePassword = $UserObfuscatedPassword | ConvertTo-SecureString -Key $AESKey
-$credential = New-Object System.Management.Automation.PsCredential($UserName, $SecurePassword)
+    #STEP 3 - Store as useable credentials; converts encrypted key into secure key for use (used in the script)
+    $UserName = 'IntuneAdmin'
+    $SecurePassword = $UserObfuscatedPassword | ConvertTo-SecureString -Key $AESKey
+    $credential = New-Object System.Management.Automation.PsCredential($UserName, $SecurePassword)
 
-#STEP 4 - Test password output (clear text) from creds
-$credential.GetNetworkCredential().password
+    #STEP 4 - Test password output (clear text) from creds
+    $credential.GetNetworkCredential().password
 
 #>
 
@@ -80,8 +79,8 @@ If(-Not $LocalAdmin)
 }
 Else{
     Try{
-        Write-Host ("Updating new account [{0}] with new password" -f $UserName) -NoNewline
-        Set-LocalUser -Name $UserName -Password $SecurePassword -Description $Description -AccountNeverExpires -PasswordNeverExpires:$true -UserMayChangePassword:$false
+        Write-Host ("Updating new account [{0}] with new password..." -f $UserName) -NoNewline
+        Set-LocalUser -Name $UserName -Password $SecurePassword -Description $Description -AccountNeverExpires -PasswordNeverExpires:$true -UserMayChangePassword:$false -ErrorAction Stop
         Write-Host ("Done") -ForegroundColor Green
     }
     Catch{
@@ -96,9 +95,10 @@ $AdminGroup = Get-WmiObject -Query $query
 #if user is not in the group, add it
 If($UserName -notin $AdminGroup.Name)
 {
+    Write-Host ("Adding account [{0}] to administrators group..." -f $UserName) -NoNewline
     Add-LocalGroupMember -Group "Administrators" -Member $UserName -ErrorAction SilentlyContinue
-    Write-Host ("Adding account [{0}] to administrators group" -f $UserName)
+    Write-Host ("Done") -ForegroundColor Green
 }
 Else{
-    Write-Host ("Account [{0}] is already in the administrators group" -f $UserName)
+    Write-Host ("Account [{0}] is already in the administrators group" -f $UserName) -ForegroundColor Green
 }
